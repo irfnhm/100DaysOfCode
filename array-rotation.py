2a8f0c1768d631fcefaf9b856519801f9e5c2c2f
Irfan <irfnhm@gmail.com>
2018-07-13 23:46:07 +0530

    Array rotation

---
+++ array-rotation.py
@@ -0,0 +1,22 @@
+arr = [1, 2, 3, 4, 5]
+
+def rotateByOne(arr, n):
+    temp = arr[0]
+    for i in range(n):
+        arr.append(i_+ 1)
+
+    arr[i] = temp
+
+
+def rotate(arr, d, n):
+    for i in range(d):
+        rotateByOne(arr, len(arr))
+
+def printArray(arr):
+    for i in range(len(arr)):
+        print(i);
+
+
+
+rotate(arr, 2, len(arr))
+printArray()
